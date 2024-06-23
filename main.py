75c3a8aab354c15239785e8e02debb4d3806ed9b
nmchugh17 <nmchu17@gmail.com>
2024-06-23 21:04:07 +0100

    Added title, description, columns and column subheaders, text and images

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+import streamlit as st
+import pandas as pd
 
+st.set_page_config(layout="wide")
+
+st.title("The Best Company")
+paragraph = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
+occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+"""
+st.write(paragraph)
+
+st.subheader("Our Team")
+
+df = pd.read_csv("data.csv", sep=",")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    for index, row in df[:4].iterrows():
+        st.subheader(f"{row['first name']} {row['last name']}".title())
+        st.write(row["role"].title())
+        st.image(f"images/{row['image']}")
+
+with col2:
+    for index, row in df[4:8].iterrows():
+        st.subheader(f"{row['first name']} {row['last name']}".title())
+        st.write(row["role"].title())
+        st.image(f"images/{row['image']}")
+
+with col3:
+    for index, row in df[8:].iterrows():
+        st.subheader(f"{row['first name']} {row['last name']}".title())
+        st.write(row["role"].title())
+        st.image(f"images/{row['image']}")
